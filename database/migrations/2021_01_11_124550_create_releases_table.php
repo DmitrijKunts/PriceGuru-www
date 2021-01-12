bfcb2c64c9f6f121008bd66704c5e04e6f7f0ac1
DmitrijKunts <r206265@gmail.com>
2021-01-12 13:22:51 +0200

    init

---
+++ database/migrations/2021_01_11_124550_create_releases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReleasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('releases', function (Blueprint $table) {
+            $table->id();
+            $table->integer('version');
+            $table->string('description');
+            $table->string('file_inst');
+            $table->string('file_arc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('releases');
+    }
+}
