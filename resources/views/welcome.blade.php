e89025927516e2a8100c5750d24e5d9b16ca0d96
Dmitrij Kunts <r206265@gmail.com>
2021-08-19 15:52:49 +0300

    seo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
                 <div class="bg-white shadow overflow-hidden sm:rounded-lg">
                     <div class="px-4 py-5 sm:px-6">
                         <h3 class="text-lg leading-6 font-medium text-gray-900">
-                            Программа <b>Price-Guru</b> предназначена для обработки прайс-листов сформированных в
+                            Программа <b>Price-Guru</b> предназначена для анализа и обработки прайс-листов поставщиков сформированных в
                                 <a itemscope itemtype="https://schema.org/Brand" target="_blank" href="https://ru.wikipedia.org/wiki/Microsoft_Excel">MS Excel'е</a> или
                                 <a itemscope itemtype="https://schema.org/Brand" target="_blank" href="https://ru.wikipedia.org/wiki/OpenOffice_Calc">OpenOffice Calc</a>.
                         </h3>
