057945c651e2bb550015c7c334683d72a2c67dde
Dmitrij Kunts <r206265@gmail.com>
2021-11-17 11:10:17 +0200

    видео регистрации

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -20,7 +20,6 @@
 
                     <div class="pt-6">
                         <dt class="text-lg">
-
                             <button type="button" class="text-left w-full flex justify-between items-start text-gray-400"
                                 aria-controls="faq-0" aria-expanded="false">
                                 <span class="font-medium text-gray-900">
@@ -43,6 +42,29 @@
                         </dd>
                     </div>
 
+                    <div class="pt-6">
+                        <dt class="text-lg">
+                            <button type="button" class="text-left w-full flex justify-between items-start text-gray-400"
+                                aria-controls="faq-0" aria-expanded="false">
+                                <span class="font-medium text-gray-900">
+                                    2. Регистраци программы
+                                </span>
+                                <span class="ml-6 h-7 flex items-center">
+
+                                    <svg class="rotate-0 h-6 w-6 transform" xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M19 9l-7 7-7-7" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </dt>
+                        <dd class="mt-2 pr-12" id="faq-0">
+                            <div class="aspect-w-16 aspect-h-9">
+                                <object class="lozad" data-data="//www.youtube.com/embed/Gq1QgfM8Jm8"></object>
+                            </div>
+                        </dd>
+                    </div>
 
 
                 </dl>
