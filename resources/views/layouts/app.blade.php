ec73ae38d4933aaa7f92671f81e1cc10c0264363
Dmitrij Kunts <r206265@gmail.com>
2021-11-17 19:30:13 +0200

    debug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 <body class="font-sans antialiased">
 
     <div class="min-h-screen bg-gray-100">
-        @livewire('navigation-menu')
+        {{-- @livewire('navigation-menu') --}}
 
         <!-- Page Heading -->
         <header class="bg-white shadow">
