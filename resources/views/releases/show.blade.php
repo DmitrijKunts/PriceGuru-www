ec5041801c75892703230e4d1e587bb0dbd68591
Dmitrij Kunts <r206265@gmail.com>
2021-11-18 17:55:19 +0200

    bug

--- resources/views/releases/show.blade.php
+++ resources/views/releases/show.blade.php
@@ -61,7 +61,7 @@
                                     Примечание
                                 </dt>
                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                    {{ $release->description }}
+                                    {!! nl2br($release->description) !!}
                                 </dd>
                             </div>
                             <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
